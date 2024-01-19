3284d990d38f895c2b6c34302a944e2aa8afd2d9
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-01-20 01:02:35 +0900

    basic syntax

--- src/isbn-4297134195/chapter3/basicSyntax/main.go
+++ src/isbn-4297134195/chapter3/basicSyntax/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-	
+	// declare variable
+	var l int
+	var m int = 10
+	n := 20
+	const x = 10
+
+	fmt.Println(l, m, n)
+
+	// strings from two strings
+	var s string = strconv.Itoa(m) + strconv.Itoa(n)
+	fmt.Println(s)
 }
