42891cb4fcaabd39a6a7bf0214b3f81dd523252b
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-01-26 01:04:22 +0900

    lift up cap of slice in case of appending more times than original cap of slice

--- src/isbn-4297134195/chapter3/listSlice/main.go
+++ src/isbn-4297134195/chapter3/listSlice/main.go
@@ -16,4 +16,13 @@ func main() {
 
 	fmt.Println(a)
 	fmt.Println(b)
+
+	// slice with len and cap
+	c := make([]int, 0, 100)
+	for i := 0; i < 200; i++ {
+		c = append(c, i)
+		if i % 10 == 0 {
+			fmt.Println(len(c), cap(c))
+		}
+	}
 }
