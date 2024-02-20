34be2281a0905f85a60c544c9e192ed088391821
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-20 23:53:01 +0900

    struct

--- src/isbn-4297134195/chapter3/struct/main.go
+++ src/isbn-4297134195/chapter3/struct/main.go
@@ -8,9 +8,14 @@ type User struct {
 }
 
 func main() {
+	// basic struct usage
 	var user User
 	user.Name = "Bob"
 	user.Age  = 20
 
+	// print struct without pointer. The value is copied every time.
 	fmt.Printf("%#v\n", user)
+
+	// print struct with pointer. The value is not copied.
+	fmt.Printf("%#v\n", &user)
 }
