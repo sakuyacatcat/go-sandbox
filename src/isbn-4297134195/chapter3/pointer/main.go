cbe5149e8854d7f00a3adb7675b96d2986ce2b01
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-24 11:40:24 +0900

    pointer receiver method

--- src/isbn-4297134195/chapter3/pointer/main.go
+++ src/isbn-4297134195/chapter3/pointer/main.go
@@ -7,10 +7,17 @@ type User struct {
 	Age int
 }
 
+type Value int
+
+func (u *User) AddAge(n Value) {
+	u.Age += int(n)
+}
+
 func main() {
 	user := new(User)
 	user.Name = "Taro"
 	user.Age = 20
+	user.AddAge(5)
 	fmt.Println(user)
 	fmt.Println(&user)
 	fmt.Println(*user)
