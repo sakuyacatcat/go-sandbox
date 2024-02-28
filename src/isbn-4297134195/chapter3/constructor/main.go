25fd83af786d6c18d8f22e51a7dbd9e4189f5d6d
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-02-28 17:55:54 +0900

    implement constructor

--- src/isbn-4297134195/chapter3/constructor/main.go
+++ src/isbn-4297134195/chapter3/constructor/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	Name string
 	Age int
@@ -10,4 +12,9 @@ func NewUser(name string, age int) *User {
 		Name: name,
 		Age: age,
 	}
+}
+
+func main() {
+	user := NewUser("Taro", 20)
+	fmt.Println(*user)
 }
