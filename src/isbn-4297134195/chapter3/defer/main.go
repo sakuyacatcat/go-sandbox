6c585a1801d227e5c687137dadaa904e636fc948
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-03 23:39:42 +0900

    defer

---
+++ src/isbn-4297134195/chapter3/defer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var b [512]byte
+	n, err := f.Read(b[:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b[:n]))
+}
