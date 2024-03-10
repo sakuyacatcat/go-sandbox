3687a7c4debfbb61213debdb7a19670021ac602a
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-11 00:16:10 +0900

    confirm capture result by execute multiple defer

--- src/isbn-4297134195/chapter3/defer/main.go
+++ src/isbn-4297134195/chapter3/defer/main.go
@@ -4,15 +4,34 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 )
 
 func main() {
-	f, err := os.Open("data.txt")
+	basePath := path.Dir("src/isbn-4297134195/chapter3/defer/main.go")
+	f, err := os.Open(path.Join(basePath, "data.txt"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+	printFile(f)
 
+	f, err = os.Open(path.Join(basePath, "data1.txt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	printFile(f)
+
+	f, err = os.Open(path.Join(basePath, "data2.txt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	printFile(f)
+}
+
+func printFile(f *os.File) {
 	var b [512]byte
 	n, err := f.Read(b[:])
 	if err != nil {
