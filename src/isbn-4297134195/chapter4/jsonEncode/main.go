90302567458deb5498e437694b7572227e035a58
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-03-30 23:46:38 +0900

    json

---
+++ src/isbn-4297134195/chapter4/jsonEncode/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+var content = `{
+	"Name": "Alice",
+	"Data": {
+		"Age": 28,
+		"Job": "Engineer",
+		"Skills": ["C", "Go", "Java"]
+	}
+}`
+
+type UserData struct {
+	Age int `json:"Age"`
+	Job string `json:"Job"`
+	Skills []string `json:"Skills"`
+}
+
+type User struct {
+	Name string `json:"Name"`
+	Data UserData `json:"Data"`
+}
+
+func main() {
+	var user User
+	err := json.Unmarshal([]byte(content), &user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", user)
+}
