7d105b0ee4a5d29e79fd4e6ee8ae5b0cb82cbeb3
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-04-01 00:43:16 +0900

    time package

---
+++ src/isbn-4297134195/chapter4/time/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	fmt.Println(t.Format(time.RFC3339))
+
+	ts := "2024/04/01 00:35:25"
+	tt, err := time.Parse("2006/01/02 15:04:05",ts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tt.Format(time.RFC3339))
+
+	td, err := time.ParseDuration("1h30m")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(td)
+	fmt.Println(td * 3)
+}
