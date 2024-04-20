9d64328d9aa02cb1bd9b269ff90c1a9982217f01
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-04-21 01:27:37 +0900

    functional options pattern example

--- src/isbn-4297134195/chapter4/functionalOptionsPattern/main.go
+++ src/isbn-4297134195/chapter4/functionalOptionsPattern/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
-	"github.com/sakuyacatcat/go-sandbox/src/isbn-4297134195/chapter4/functionOptionsPattern/server"
+	"github.com/sakuyacatcat/go-sandbox/src/isbn-4297134195/chapter4/functionalOptionsPattern/server"
 )
 
+
 func main() {
-	svr := server.New("localhost", 8888)
+	f, err := os.Create("server.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	logger := log.New(f, "", log.LstdFlags)
+	svr := server.New("localhost", 8888,
+		server.WithTimeout(time.Minute),
+		server.WithLogger(logger),
+	)
+
 	if err := svr.Start(); err != nil {
 		log.Fatal(err)
 	}
--- src/isbn-4297134195/chapter4/functionalOptionsPattern/server/server.go
+++ src/isbn-4297134195/chapter4/functionalOptionsPattern/server/server.go
@@ -1,17 +1,48 @@
 package server
 
+import (
+	"log"
+	"time"
+)
+
 type Server struct {
 	host string
 	port int
+	timeout time.Duration
+	logger *log.Logger
 }
 
-func New(host string, port int) *Server {
-	return &Server{
-		host: host,
-		port: port,
+type Option func(*Server)
+
+func WithTimeout(timeout time.Duration) func(*Server) {
+	return func(s *Server) {
+		s.timeout = timeout
 	}
 }
 
+func WithLogger(logger *log.Logger) func(*Server) {
+	return func(s *Server) {
+		s.logger = logger
+	}
+}
+
+func New(host string, port int, options ...Option) *Server {
+	svr := &Server{
+		host: host,
+		port: port,
+	}
+
+	for _, opt := range options {
+		opt(svr)
+	}
+
+	return svr
+}
+
 func (s *Server) Start() error {
+	if s.logger != nil {
+		s.logger.Println("server started")
+	}
+
 	return nil
 }
